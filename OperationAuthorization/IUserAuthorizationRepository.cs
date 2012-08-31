77025de8364c50bc708075323555c2ff5c553dff
William Liska <github@liska.me>
2012-08-31 11:03:06 -0400

    Added an interface for retrieving user authorizations.

---
+++ OperationAuthorization/IUserAuthorizationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationAuthorization
+{
+    /// <summary>
+    /// Implementations of this interface will be responsible for returning a collection of IUserAuthorizations for a given user.
+    /// </summary>
+    /// <typeparam name="T">Represents a user. Can be of whatever type is required by the implementation of this interface.</typeparam>
+    public interface IUserAuthorizationRepository<in T>
+    {
+        IEnumerable<IUserAuthorization> GetAuthorizationsFor(T user);
+    }
+}
