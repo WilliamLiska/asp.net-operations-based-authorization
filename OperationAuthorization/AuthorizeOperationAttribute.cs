3ea727ab67bfd59f3979c8c58d1625ce9e7f2889
William Liska <github@liska.me>
2012-08-31 08:29:23 -0400

    More refactoring.

--- OperationAuthorization/AuthorizeOperationAttribute.cs
+++ OperationAuthorization/AuthorizeOperationAttribute.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using System.Web;
+using System.Web.Routing;
 
 namespace OperationAuthorization
 {
@@ -38,5 +40,33 @@ namespace OperationAuthorization
         }
 
         #endregion
+
+        #region Utilities
+
+        private void ParseRouteData(RouteData routeData, out Dictionary<string, string> routeParameters, out string operation)
+        {
+            //Get only the parameters from the RouteData, excluding the controller and action
+            routeParameters = routeData.Values.Where(routeParameter => routeParameter.Key != "controller" && routeParameter.Key != "action").ToDictionary(routeParameter => routeParameter.Key, routeParameter => routeParameter.Value.ToString());
+
+            var requestedController = routeData.Values["controller"];
+            var requestedAction = routeData.Values["action"];
+
+            operation = requestedController + @"/" + requestedAction;
+        }
+
+        #endregion
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            //If, for some reason, the handler isn't an Mvc handler, give control to the base
+            if (httpContext.CurrentHandler.GetType() != typeof(System.Web.Mvc.MvcHandler))
+            {
+                base.AuthorizeCore(httpContext);
+            }
+
+
+        }
     }
+
+
 }
